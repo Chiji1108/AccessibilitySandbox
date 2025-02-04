b2f9a1df5a1a879d32e17b7cc208758d353a5dc4
chiji1108 <shingo.pad@gmail.com>
2025-02-04 14:15:27 +0900

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by 千々岩真吾 on 2025/02/04.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
